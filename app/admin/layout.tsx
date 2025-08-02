58c0d09b73d4bb8f0e3d389d5162ae8bd952c5e6
sagorsohel <sohel19sagor@gmail.com>
2025-08-03 00:19:31 +0600

    Refactor admin layout to integrate SidebarProvider and SiteHeader; streamline dashboard page structure

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "../globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+import { SiteHeader } from "@/components/site-header"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,7 +24,21 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <SidebarProvider
+            style={
+              {
+                "--sidebar-width": "calc(var(--spacing) * 72)",
+                "--header-height": "calc(var(--spacing) * 12)",
+              } as React.CSSProperties
+            }
+          >
+            <AppSidebar variant="inset" />
+            <SidebarInset>
+              <SiteHeader />
+              {children}
+            </SidebarInset>
+          </SidebarProvider>
+
         </ThemeProvider>
       </body>
     </html>
