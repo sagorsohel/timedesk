38bcc06dbecbe0e8591059e4a7889b52b4ecbeb0
sagorsohel <sohel19sagor@gmail.com>
2025-08-05 03:45:33 +0600

    Refactor sign-in page to use LoginForm component; update layout for improved responsiveness and remove unused placeholder assets and global styles.

--- app/sign-in/[[...index]]/page.tsx
+++ app/sign-in/[[...index]]/page.tsx
@@ -1,10 +1,26 @@
-import { SignIn } from '@clerk/nextjs'
+import { LoginForm } from "@/components/auth/log-in"
+import { GalleryVerticalEnd } from "lucide-react"
 
-export default function Page() {
+
+
+export default function LoginPage() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <SignIn />
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/placeholder.svg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
     </div>
   )
-
 }
---
+++ components/auth/log-in.tsx
@@ -0,0 +1,55 @@
+'use client'
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import Link from "next/link"
+import { useActionState } from "react"
+import { Loader2 } from "lucide-react"
+
+export function LoginForm({ className, ...props }: React.HTMLAttributes<HTMLFormElement>) {
+  const handleSubmit = async (prevState: any, formData: FormData) => {
+    const email = formData.get('email')
+    const password = formData.get('password')
+    console.log(email, password)
+  }
+
+  const [data, action, isPending] = useActionState(handleSubmit, undefined)
+
+
+  return (
+    <form action={action} className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold">Log In to your account</h1>
+        <p className="text-muted-foreground text-sm text-balance">
+          Enter your email below to Log In to your account
+        </p>
+      </div>
+      <div className="grid gap-6">
+        <div className="grid gap-3">
+          <Label htmlFor="email">Email</Label>
+          <Input name="email" id="email" type="email" placeholder="sohel@example.com" required />
+        </div>
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="password">Password</Label>
+
+          </div>
+          <Input name="password" id="password" type="password" required />
+        </div>
+        
+        <Button type="submit" className="w-full" disabled={isPending}>
+          {isPending ? <Loader2 className="w-4 h-4 animate-spin" /> : "Sign In"}
+        </Button>
+
+
+      </div>
+      <div className="text-center text-sm">
+        Don&apos;t have an account?{" "}
+        <Link href="/sign-up" className="underline text-primary underline-offset-4">
+          Sign up
+        </Link>
+      </div>
+    </form>
+  )
+}
